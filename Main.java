35be5f3d33d6241381fe20e4e8a0d63940643784
Karolis_portatil <karolisjakas04@gmail.com>
2024-02-09 21:28:47 +0100

    Third commit

--- Main.java
+++ Main.java
@@ -4,90 +4,139 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int eleccion;
+        MiniTrivial mt = new MiniTrivial();
 
         System.out.println("Bienvenido a MiniTrivial elige una opcion:");
 
         do {
-            System.out.println("1. Añadir nuevas preguntas");
+            System.out.println("\n1. Añadir nuevas preguntas");
             System.out.println("2. Listar todas las preguntas");
             System.out.println("3. Buscar pregunta");
             System.out.println("4. Modificar pregunta");
             System.out.println("5. Borrar pregunta");
             System.out.println("6. Elegir una pregunta al azar");
             System.out.println("7. Salir del programa");
-            System.out.print("Seleccione una opción: ");
+            System.out.println("Seleccione una opción: ");
             eleccion = sc.nextInt();
 
             switch (eleccion) {
                 case 1:
-                int tipo = 0;
-                char[] pregunta = new char[1024];
-                int categoria = 0;
-                String texto = "";
+                    int tipo = 0;
+                    char[] pregunta = new char[1024];
+                    int categoria = 0;
+                    String texto = "";
+                    String respuesta = "";
+                    String[] respuestas = null;
 
-                    System.out.println("Hay 3 tipos de pregunta:\n 1.Respuesta simple\n 2.Respuesta multiple\n 3.Respuesta si/no\nIngrese el tipo de pregunta:");
-                    do{
+                    for(int i = 0; i < 1024; i++){
+                        pregunta[i] = ' ';
+                    }
+
+                    System.out.println("\nHay 3 tipos de pregunta:\n 1.Respuesta simple\n 2.Respuesta multiple\n 3.Respuesta si/no\nIngrese el tipo de pregunta:");
+                    do {
                         tipo = sc.nextInt();
-                        if(tipo < 1 || tipo > 3) System.out.println("Escriba un numero del 1 al 3!");
+                        if (tipo < 1 || tipo > 3)
+                            System.out.println("Escriba un numero del 1 al 3!");
                     } while (tipo < 1 || tipo > 3);
 
-                    do{
+                    do {
 
                         System.out.println("Ingrese la pregunta: ");
-                        texto = sc.next();
-                        if(texto.length() > 1024) System.out.println("La pregunta no puede tener más de 1024 caracteres!");
+                        texto = sc.nextLine();
+                        if (texto.length() > 1024)
+                            System.out.println("La pregunta no puede tener más de 1024 caracteres!");
 
-                    } while(texto.length() > 1024);
+                    } while (texto.length() > 1024);
 
-                    for(int i = 0; i < texto.length(); i++){
+                    sc.nextLine();
+
+                    for (int i = 0; i < texto.length(); i++) {
                         pregunta[i] = texto.charAt(i);
                     }
 
-                    System.out.println("Ingrese la categoría de la pregunta hay 6: ");
-                    do{
+                    System.out.println("\nIngrese la categoría de la pregunta hay 6: ");
+                    do {
                         categoria = sc.nextInt();
-                        if(tipo < 1 || tipo > 6) System.out.println("Escriba un numero del 1 al 6!");
-                    } while(categoria < 1 || categoria > 6);
-                    
-                    switch(tipo){
+                        if (tipo < 1 || tipo > 6)
+                            System.out.println("Escriba un numero del 1 al 6!");
+                    } while (categoria < 1 || categoria > 6);
+
+                    switch (tipo) {
                         case 1:
+                            respuestas = new String[1];
                             System.out.println("1.Respuesta simple\nIngresa la respuesta: ");
-                            String respuesta = sc.next();
-                            char[] respuestaChar = new char[1024];
-                            for(int i = 0; i < respuesta.length(); i++){
-                                respuestaChar[i] = respuesta.charAt(i);
-                            }
+                            do {
+                                respuesta = sc.next();
+                                if (respuesta.length() > 1024)
+                                    System.out.println("La respuesta no puede tener más de 1024 caracteres!");
+                            } while (respuesta.length() > 1024);
+
+                            respuestas[0] = respuesta;
 
                             break;
                         case 2:
+                            respuestas = new String[3];
+                            int aleatorio = (int) (Math.random() * 3);
+
                             System.out.println("2.Respuesta multiple\nIngrese la respuesta correcta: ");
-                            String respuestaCorrecta = sc.next();
-                            char[] respuestaCorrectaChar = new char[1024];
+                            do {
+                                respuesta = sc.next() + "*";
+                                if (respuesta.length() > 1024)
+                                    System.out.println("La respuesta no puede tener más de 1024 caracteres!");
 
-                            for(int i = 0; i < respuestaCorrecta.length(); i++){
-                                respuestaCorrectaChar[i] = respuestaCorrecta.charAt(i);
+                            } while (respuesta.length() > 1024);
+
+                            respuestas[aleatorio] = respuesta;
+
+                            System.out.println("Ingrese la respuesta incorrecta 1: ");
+                            do {
+                                respuesta = sc.next();
+                                if (respuesta.length() > 1024)
+                                    System.out.println("La respuesta no puede tener más de 1024 caracteres!");
+
+                            } while (respuesta.length() > 1024);
+
+                            for (int i = 0; i < 3; i++) {
+                                if (respuestas[i] == null) {
+                                    respuestas[i] = respuesta;
+                                    i = 3;
+                                }
                             }
 
-                            System.out.println("Ingrese la respuesta incorrecta: ");
-                            String respuestaIncorrecta = sc.next();
-                            char[] respuestaIncorrectaChar = new char[1024];
-                            for(int i = 0; i < respuestaIncorrecta.length(); i++){
-                                respuestaIncorrectaChar[i] = respuestaIncorrecta.charAt(i);
+                            System.out.println("Ingrese la respuesta incorrecta 2: ");
+                            do {
+                                respuesta = sc.next();
+                                if (respuesta.length() > 1024)
+                                    System.out.println("La respuesta no puede tener más de 1024 caracteres!");
+
+                            } while (respuesta.length() > 1024);
+
+                            for (int i = 0; i < 3; i++) {
+                                if (respuestas[i] == null) {
+                                    respuestas[i] = respuesta;
+                                    i = 3;
+                                }
                             }
-                            
+
                             break;
                         case 3:
+                            respuestas = new String[1];
                             System.out.println("Ingrese la respuesta correcta: ");
-                            String respuestaCorrectaSN = sc.next();
-                            char[] respuestaCorrectaSNChar = new char[1024];
-                            for(int i = 0; i < respuestaCorrectaSN.length(); i++){
-                                respuestaCorrectaSNChar[i] = respuestaCorrectaSN.charAt(i);
-                            }
+                            do {
+                                respuesta = sc.next();
+                                if (!respuesta.equals("si") || !respuesta.equals("no"))
+                                    System.out.println("La respuesta debe ser si o no!");
+                            } while (!respuesta.equals("si") || !respuesta.equals("no"));
+
+                            respuestas[0] = respuesta;
                             break;
                     }
 
+                    guardarPregunta(pregunta, tipo, categoria, false, respuestas);
+
                     break;
                 case 2:
+                    System.out.println(mt.leer());
                     break;
                 case 3:
                     break;
@@ -104,13 +153,13 @@ public class Main {
                     System.out.println("Opción inválida. Intente nuevamente.");
                     break;
             }
-        } while (eleccion != 7);        
+        } while (eleccion != 7);
     }
 
-    public static void guardarPregunta(String tipo, String pregunta, boolean borrada, String categoria) {
+    public static void guardarPregunta(char[] pregunta, int tipo, int categoria, boolean borrado, String[] respuesta) {
         MiniTrivial mt = new MiniTrivial();
 
-        mt.guardar(null, 0, 0, borrada, null);
+        mt.guardar(pregunta, tipo, categoria, borrado, respuesta);
     }
 
 }
--- MiniTrivial.java
+++ MiniTrivial.java
@@ -1,33 +1,46 @@
 import java.io.RandomAccessFile;
 
 public class MiniTrivial {
-    private int contador = 0;
+    private int contador = 1;
+    private int busqueda = 0;
 
-     public void guardar(char[] pregunta, int tipo, int categoria, boolean borrado, char[] respuesta){
+     public void guardar(char[] pregunta, int tipo, int categoria, boolean borrado, String[] respuesta){
         
         try{    
             RandomAccessFile WriterPreg = new RandomAccessFile("Preguntas.dat","rw");
-            
-                WriterPreg.write(contador);
+                WriterPreg.seek(contador * 0);
+                WriterPreg.writeInt(contador);
 
-                WriterPreg.write(tipo);
+                WriterPreg.seek(contador * 4);
+                WriterPreg.writeInt(tipo);
 
-                WriterPreg.write(categoria);
+                WriterPreg.seek(contador * 8);
+                WriterPreg.writeInt(categoria);
 
-                for(int i = 0; i < pregunta.length; i++){
+                if(contador == 1) busqueda = 12;
+                else busqueda = 1036;
+
+                for(int i = 0; i < 1024; i++){
+                    WriterPreg.seek(contador * busqueda + i);
                     WriterPreg.writeChar(pregunta[i]);
                 }
 
+                WriterPreg.seek(contador * 1036);
                 WriterPreg.writeBoolean(borrado);
                 
             WriterPreg.close();
 
             RandomAccessFile WriterResp = new RandomAccessFile("Respuestas.dat","rw");      
             
+                WriterResp.seek(contador * 0);
                 WriterResp.write(contador);
 
+                WriterResp.seek(contador * 12);
                 for(int i = 0; i < respuesta.length; i++){
-                    WriterResp.writeChar(respuesta[i]);
+                    for(int j = 0; j < 1024; j++){
+                        if(respuesta[i] != null && respuesta[i].charAt(j) != '\u0000') WriterResp.writeChar(respuesta[i].charAt(j));
+                    }
+                    if(respuesta.length > 1) WriterResp.writeChar('/');
                 }
 
             WriterResp.close();
@@ -41,15 +54,31 @@ public class MiniTrivial {
 
     public String leer(){
         String pregunta = "";
+        System.out.println(contador);
         try{
             RandomAccessFile ReaderPreg = new RandomAccessFile("Preguntas.dat","r");
-            int tipo = ReaderPreg.readInt();
-            int categoria = ReaderPreg.readInt();
+            ReaderPreg.seek(contador * 0);
+
+            pregunta += "id: " + ReaderPreg.readInt() + " ";
+
+            ReaderPreg.seek(contador * 4);
+            pregunta += " tipo: " + ReaderPreg.readInt() + " ";
+
+            ReaderPreg.seek(contador * 8);
+            pregunta += " categoria: " + ReaderPreg.readInt() + " ";
+            
+            pregunta += " pregunta: ";
+
+            if(contador == 1) busqueda = 12;
+            else busqueda = 1036;
+
             for(int i = 0; i < 1024; i++){
                 if(ReaderPreg.readChar() == ' ') i = 1025;
+
+                ReaderPreg.seek(contador * busqueda + i);
                 pregunta += ReaderPreg.readChar();
             }
-            boolean borrado = ReaderPreg.readBoolean();
+
             ReaderPreg.close();
 
         }
